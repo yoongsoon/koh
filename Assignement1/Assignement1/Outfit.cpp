6652b65bbdd738ef8483d169bd6ef811cbd6d383
yoongsoon <yoongsoon@live.com>
2016-11-27 18:08:29 +0800

    added member definition for outfit

--- Assignement1/Assignement1/Outfit.cpp
+++ Assignement1/Assignement1/Outfit.cpp
@@ -1,2 +1,19 @@
 #include "Outfit.h"
 
+
+Outfit::Outfit(const string & nameOfOutfit, const int & initDurability, const int & outfitSPECIAL)   
+	:Item(nameOfOutfit, initDurability), kSPECIAL(outfitSPECIAL)
+{
+
+	
+}
+
+Outfit::~Outfit()
+{
+
+}
+
+int Outfit::getSPECIAL() const
+{
+	return kSPECIAL;
+}
